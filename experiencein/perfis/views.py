a54879e7b63c5e10cccd5b4d605040ab167e5a16
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-10-05 11:47:52 -0300

    mudando views.py para buscar um perfil do BD

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -5,13 +5,6 @@ from perfis.models import Perfil
 def index(request):
     return render(request, 'index.html')
 
-def exibir(request, perfil_id):
-
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fabio Henrique', 'fabio.oliveira@ifb.edu.br','222222','IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla')
-   
+def exibir(request, perfil_id):    
+    perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {'perfil' : perfil})
