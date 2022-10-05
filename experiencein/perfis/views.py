03db8c66a8395bdf0397628d75715e252af5d504
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-10-05 09:23:51 -0300

    criando view perfis; registrando nova rota

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def exibir(request):
+    return render(request, 'perfil.html')
