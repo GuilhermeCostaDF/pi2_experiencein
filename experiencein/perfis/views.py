ad420d169c97aed4bd11557f73c7cc194c3d8b33
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-10-05 10:17:39 -0300

    confg views.py para acessar perfil especifico

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from perfis.models import Perfil
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
-def exibir(request):
-    return render(request, 'perfil.html')
+def exibir(request, perfil_id):
+
+    perfil = Perfil()
+
+    if perfil_id == 1:
+        perfil = Perfil('Fabio Henrique', 'fabio.oliveira@ifb.edu.br','222222','IFB')
+    if perfil_id == 2:
+        perfil = Perfil('Elon Musk', 'elon.musk@tesla.com', '333333', 'Tesla')
+   
+    return render(request, 'perfil.html', {'perfil' : perfil})
