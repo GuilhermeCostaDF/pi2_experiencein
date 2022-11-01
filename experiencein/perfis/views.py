8a42bf53549ef2e76c322cc4f57379c9cbddf388
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-01 15:06:50 -0300

    ajustando view

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -7,8 +7,8 @@ from django.shortcuts import redirect
 def index(request):
     return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
-def exibir(request, perfil_id):
-    # código comentado
+def exibir(request, perfil_id):    
+    perfil = Perfil.objects.get(id=perfil_id)
     return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar(request, perfil_id):
@@ -20,3 +20,4 @@ def convidar(request, perfil_id):
 
 def get_perfil_logado(request):
     return Perfil.objects.get(id=1)
+
