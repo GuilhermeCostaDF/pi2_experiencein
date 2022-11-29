ef799a2c9c263d8366bb2972cce4645d4ee7d159
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-11-29 09:28:24 -0300

    adicionando permissoes

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render, redirect
 from perfis.models import Perfil, Convite
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
+@login_required
 def index(request):
-	return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
+    print(request.user.username) #novo
+    print(request.user.email) #novo
+    print(request.user.has_perm('perfis.add_convite')) #novo
 
-def exibir(request, perfil_id):	
-	perfil = Perfil.objects.get(id=perfil_id)
+    return render(request,'index.html',{'perfis': Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request) })
 
-	perfil_logado = get_perfil_logado(request)
-	ja_e_contato = perfil in perfil_logado.contatos.all()
-	
-	return render(request, 'perfil.html', {'perfil':perfil, "ja_e_contato":ja_e_contato})
+@login_required
+def exibir(request, perfil_id):
 
+    perfil = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    ja_e_contato = perfil in perfil_logado.contatos.all()
+
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_e_contato' : ja_e_contato})
+
+@login_required
+@permission_required('perfis.add_convite', raise_exception=True)
 def convidar(request, perfil_id):
-	perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-	perfil_logado = get_perfil_logado(request)
-	perfil_logado.convidar(perfil_a_convidar)
-	return redirect('index')
 
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+    return redirect('index')
+
+@login_required
 def aceitar(request, convite_id):
-	convite = Convite.objects.get(id=convite_id)
-	convite.aceitar()
-	return redirect('index')
+    convite = Convite.objects.get(id=convite_id)
+    convite.aceitar()
+    return redirect('index')
 
+@login_required
 def get_perfil_logado(request):
-	return Perfil.objects.get(id=1)
+    return request.user.perfil
