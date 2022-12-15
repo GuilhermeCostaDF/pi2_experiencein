839661ab1bab1517354f630235c1997fc313199c
GuilhermeCostaDF <guidocostadf@gmail.com>
2022-12-15 14:18:26 -0300

    alterando o arquivo views.py para funcionar como uma api

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -71,38 +71,3 @@ def get_meu_perfil(request, *args, **kwargs):
 
 def get_perfil_logado(request):
     return request.user.perfil
-
-
-@login_required
-def index(request):
-    print(request.user.username)  # novo
-    print(request.user.email)  # novo
-    print(request.user.has_perm('perfis.add_convite'))  # novo
-
-    return render(request, 'index.html', {'perfis': Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
-
-
-@login_required
-def exibir(request, perfil_id):
-
-    perfil = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    ja_e_contato = perfil in perfil_logado.contatos.all()
-
-    return render(request, 'perfil.html', {'perfil': perfil, 'perfil_logado': get_perfil_logado(request), 'ja_e_contato': ja_e_contato})
-
-
-@login_required
-def convidar(request, perfil_id):
-
-    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
-    perfil_logado = get_perfil_logado(request)
-    perfil_logado.convidar(perfil_a_convidar)
-    return redirect('index')
-
-
-@login_required
-def aceitar(request, convite_id):
-    convite = Convite.objects.get(id=convite_id)
-    convite.aceitar()
-    return redirect('index')
